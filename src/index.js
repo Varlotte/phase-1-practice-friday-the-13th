c3cfec1ac4ad3637403d258f6339a98b5083462c
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-08 11:31:07 -0400

    sets up the event listeners for the form and blood count

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 baseUrl = "http://localhost:3000/movies";
 const movieList = document.getElementById("movie-list");
 document.createElement;
+let currentMovie;
+let bloodForm = document.getElementById("blood-form");
 
 fetch(baseUrl)
   .then((response) => response.json())
@@ -18,6 +20,7 @@ function renderMovie(movie) {
 }
 
 function showMovieDetails(movie) {
+  currentMovie = movie;
   let detailTitle = document.getElementById("title");
   let detailImage = document.getElementById("detail-image");
   let detailDescription = document.getElementById("description");
@@ -29,6 +32,37 @@ function showMovieDetails(movie) {
   detailImage.src = movie.image;
   detailDescription.innerText = movie.description;
   yearRelease.innerText = movie.release_year;
-  watchedButton.innerText = movie.watched;
+  watchedButton.innerText = movie.watched ? "Watched" : "Unwatched";
   bloodAmount.innerText = movie.blood_amount;
+
+  watchedButton.addEventListener("click", () => {
+    currentMovie.watched = !currentMovie.watched;
+    watchedButton.textContent = currentMovie.watched ? "Watched" : "Unwatched";
+  });
 }
+
+function bloodCounter() {
+  bloodForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const amountToAdd = e.target["blood-amount"].value;
+    currentMovie.blood_amount += parseInt(amountToAdd);
+    document.getElementById("amount").textContent = currentMovie.blood_amount;
+
+    e.target.reset();
+  });
+}
+
+bloodCounter();
+
+/*function bloodCounter() {
+  bloodForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const amountToAdd = e.target["blood-amount"].value;
+    currentMovie.bloodAmount += parseInt(amountToAdd);
+    document.getElementById("amount").textContent = currentMovie.blood_amount;
+    e.target.reset();
+  });
+}
+bloodCounter();
+*/
